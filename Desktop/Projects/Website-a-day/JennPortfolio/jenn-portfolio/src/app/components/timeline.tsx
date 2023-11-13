bb23fff1a631f0d17354b8416e2aa9a122a271e5
James Richmond <jblackenmusic@gmail.com>
2023-11-12 22:59:19 -0700

    timeline

---
+++ Desktop/Projects/Website-a-day/JennPortfolio/jenn-portfolio/src/app/components/timeline.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import React, { useEffect, useState } from 'react'
+
+interface dateItemProps {
+    dateID: number,
+    dateTitle: string,
+    dateDescription: string,
+    dateDate: string
+}
+
+export default function Timeline() {
+    return (
+        <div>
+            <
+        </div>
+    )
+}
