5a4d6667ad8d5aeecda3a8aa05f947b903f443fd
James Richmond <jblackenmusic@gmail.com>
2023-11-13 16:04:39 -0700

    added timeline

--- Desktop/Projects/Website-a-day/JennPortfolio/jenn-portfolio/src/app/page.tsx
+++ Desktop/Projects/Website-a-day/JennPortfolio/jenn-portfolio/src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
+import Timeline from './components/timeline'
 
 export default function Home() {
   return (
     <main className="">
       <div>
-        
+        <Timeline />
       </div>
     </main>
   )
